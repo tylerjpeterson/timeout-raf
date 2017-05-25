f92aec3471ac815e34fbfc743ca883bf3dc1283a
Tyler Peterson <peterson_tyler@apple.com>
2017-05-25 04:47:24 -0400

    Tape tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,74 +1,80 @@
 /* global describe, it, assert, timeoutRaf */
 'use strict';
 
+const test = require('tape');
+const timeoutRaf = require('./../../');
+
 /**
  * timeoutRaf mocha tests
  * @see https://mochajs.org/
  */
-describe('timeoutRaf', function () {
-	it('should be a function', function () {
-		assert.equal(typeof timeoutRaf, 'function');
-	});
 
-	it('should fire when expected', function(done) {
-		var t = Date.now();
-		var e = 0;
+test('should be a function', assert => {
+	assert.equal(typeof timeoutRaf, 'function');
+	assert.end();
+});
 
-		timeoutRaf(() => {
-			e = Date.now();
-			assert.closeTo(e - t, 1000, 100, 'numbers are close');
-			done();
+test('should fire when expected', assert => {
+	var t = Date.now();
+	var e = 0;
+
+	timeoutRaf(() => {
+		var diff = e - t < 100;
+		assert.equal(diff, true);
+		assert.end();
+	}, 1000);
+});
+
+test('should keep context', assert => {
+	var ctx = {
+		testprop: 'exists'
+	};
+
+	timeoutRaf(function () {
+		assert.equal(this.testprop, 'exists');
+		assert.end();
+	}, 1000, ctx);
+});
+
+test('should keep traditional context', assert => {
+	var ctx = {testprop: 'exists'};
+
+	timeoutRaf(function()  {
+		assert.equal(this.testprop, 'exists');
+		assert.end();
+	}.bind(ctx), 1000);
+});
+
+test('should be killable', assert => {
+	var v = 1;
+
+	var timeout1 = timeoutRaf(() => {
+		v = 2;
+	}, 1000);
+
+	var timeout2 = timeoutRaf(() => {
+		timeout1.kill();
+	}, 500);
+
+	var timeout3 = timeoutRaf(() => {
+		assert.equal(v, 1);
+		assert.end();
+	}, 1500);
+});
+
+test('should fire immediately when told to', assert => {
+	var v = 1;
+
+	var timeout1 = timeoutRaf(() => {
+		v = 2;
+	}, 1500);
+
+	timeoutRaf(() => {
+		timeout1.fire();
+		assert.equal(v, 2);
+		assert.end();
+		setTimeout(() => {
+			window.close();
 		}, 1000);
-	});
-
-	it('should keep context', function(done) {
-		var ctx = {
-			testprop: 'exists'
-		};
-
-		timeoutRaf(function () {
-			assert.equal(this.testprop, 'exists');
-			done();
-		}, 1000, ctx);
-	});
-
-	it('should keep traditional context', function(done) {
-		var ctx = {testprop: 'exists'};
-
-		timeoutRaf(function()  {
-			assert.equal(this.testprop, 'exists');
-			done();
-		}.bind(ctx), 1000);
-	});
-
-	it('should be killable', function(done) {
-		var v = 1;
-
-		var timeout1 = timeoutRaf(() => {
-			v = 2;
-		}, 1000);
-
-		var timeout2 = timeoutRaf(() => {
-			timeout1.kill();
-		}, 500);
-
-		var timeout3 = timeoutRaf(() => {
-			assert.equal(v, 1);
-			done();
-		}, 1500);
-	});
-
-	it('should fire immediately when told to', function(done) {
-		var v = 1;
-
-		var timeout1 = timeoutRaf(() => {
-			v = 2;
-		}, 1500);
-
-		timeoutRaf(() => {
-			timeout1.fire();
-			assert.equal(v, 2);
-			done();
-		}, 1000);
-	});
+	}, 1000);
 });
